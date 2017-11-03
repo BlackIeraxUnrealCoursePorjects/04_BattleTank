6a28e8b43322e8a055ce92bd5a40645567aeaec8
BLACK_IERAX <nikoloudisjohn@gmail.com>
2017-11-03 17:19:25 +0200

    fixed issue that crashed made a few tweaks

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -14,10 +14,19 @@ ATank::ATank()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
+
+	auto TankName = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s DONKEY: Tank c++ construct:"), *TankName);
+}
+void ATank::BeginPlay()
+{
+	//Super
+	Super::BeginPlay(); // Needed for BP begin play to run
 }
 
 void ATank::AimAt(FVector HitLocation) 
 {
+	if (!TankAimingComponent) { return; }
 	TankAimingComponent->AimAt(HitLocation, LaunchSpeed);
 }
 
