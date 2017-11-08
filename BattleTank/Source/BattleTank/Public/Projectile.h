d042d356078398be8f05bc51447c387a709ef8df
BLACK_IERAX <nikoloudisjohn@gmail.com>
2017-11-08 17:57:52 +0200

    added healthbar and aplied damadge to tank

--- BattleTank/Source/BattleTank/Public/Projectile.h
+++ BattleTank/Source/BattleTank/Public/Projectile.h
@@ -6,6 +6,7 @@
 #include "GameFramework/Actor.h"
 #include "GameFramework/ProjectileMovementComponent.h"
 #include "Runtime/Engine/Classes/Particles/ParticleSystemComponent.h"
+#include "Runtime/Engine/Classes/Kismet/GameplayStatics.h"
 #include "PhysicsEngine/RadialForceComponent.h"
 #include "Projectile.generated.h"
 
@@ -36,6 +37,8 @@ private:
 	UPROPERTY(EditDefaultsOnly, Category = "Setup")
 	float DestroyDelay = 10.f;
 
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+	float ProjectileDamage = 20.f;
 	
 	UProjectileMovementComponent* ProjectileMovement = nullptr;
 
